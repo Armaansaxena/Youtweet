7a1a6b561ea6047f4e3abc32cee084f0b16c9492
[Armaansaxena] <armaansaxena704@gmail.com>
2025-07-04 12:26:23 +0530

    some changes in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { User } from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import path from 'path'
-import fs from "fs";
 // filepath: c:\Users\armaa\OneDrive\Desktop\ChaiBackend\src\controllers\user.controller.js
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -35,9 +34,14 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
     let avatarLocalPath = req.files?.avatar?.[0]?.path;
-    let coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    let coverImageLocalPath ;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+    // let avatarLocalPath = req.files?.avatar?.[0]?.path;
+    // let coverImageLocalPath = req.files?.coverImage?.[0]?.path;
     
 
     if (avatarLocalPath) {
@@ -58,10 +62,9 @@ const registerUser = asyncHandler( async (req, res) => {
     
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-    console.log("coverImage upload result:", coverImage);
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar file is required")
+        throw new ApiError(400, "Avatar upload failed")
     }
    
 
@@ -76,7 +79,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
-    )
+    );
 
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user")
